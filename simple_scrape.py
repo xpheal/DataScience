cdb3c86368744d76dabc65a995b7991c5376f18d
xpheal <xpheal@gmail.com>
2016-10-08 06:14:00 -0500

    regex mod

--- simple_scrape.py
+++ simple_scrape.py
@@ -32,7 +32,7 @@ def get_default_settings_obj():
 	settings_obj["page_regex"] = None
 	settings_obj["randomize_download_delay"] = 0
 	settings_obj["download_delay"] = 0
-	settings_obj["depth_priority"] = 0
+	settings_obj["depth_priority"] = 1
 	settings_obj["data_extract_path"] = None
 	return settings_obj
 
@@ -41,10 +41,10 @@ def download(args):
 	settings_obj = get_default_settings_obj()
 	settings_obj["save_html_to_directory"] = True
 	settings_obj["start_urls"] = [args.url]
-	settings_obj["allowed_domains"] = [re.match('https?://(.*)', args.url).group(1)]
+	settings_obj["allowed_domains"] = [re.match('https?://([^/]+)', args.url).group(1)]
+	print(re.match('https?://([^/]+)', args.url).group(1))
 	settings_obj["html_directory_name"] = "HTMLFiles"
-	settings_obj["save_file_regex"] = "http://(.*)"
-
+	settings_obj["save_file_regex"] = "https?://(.*)"
 	start_crawler(settings_obj)
 
 def add_default_settings(settings_obj):
